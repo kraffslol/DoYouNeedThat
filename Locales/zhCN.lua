0b6bc55b868b2d0466fd25f6a2a8492b3e6320a7
Rickard Ekman <xm0rfin@gmail.com>
2020-10-21 13:12:07 +0200

    No need for namespaces

--- Locales/zhCN.lua
+++ Locales/zhCN.lua
@@ -3,4 +3,4 @@ local addonName, AddOn = ...
 local L = AddOn.L
 L = L or {}
 
---@localization(locale="zhCN", format="lua_additive_table", namespace="DYNT", handle-subnamespaces="none")@
+--@localization(locale="zhCN", format="lua_additive_table", handle-subnamespaces="none")@
